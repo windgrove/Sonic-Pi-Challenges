7cb17579207dc8e12475036abd95b9e0b3471c82
windgrove <windgrove_daniel@student.davincischools.org>
2026-03-27 11:41:11 -0700

    Create Custom Song 2.rb

---
+++ Custom Song 2.rb
@@ -0,0 +1,24 @@
+use_bpm 140
+use_synth :piano
+
+play :d2, amp: 0.5
+sleep 0.5
+play :f2, amp: 0.5
+sleep 0.5
+play :g2, sustain: 7, amp: 0.5
+sleep 1.5
+play :d3, amp: 0.5
+sleep 0.5
+play :d3, amp: 0.5
+sleep 0.5
+play :d3, amp: 0.5
+sleep 0.5
+play :d3, sustain: 1, amp: 0.5
+sleep 1.5
+play :d3
+sleep 0.5
+play :c3, sustain: 0.5, amp: 0.5
+sleep 1
+play :a2, amp: 0.5
+sleep 0.5
+play :f2, sustain: 1, amp: 0.5
