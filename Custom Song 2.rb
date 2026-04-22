853b842716fa9c40b68a03a75abea3793ef36e3d
windgrove <windgrove_daniel@student.davincischools.org>
2026-04-22 12:51:24 -0700

    Update Custom Song 2.rb

--- Custom Song 2.rb
+++ Custom Song 2.rb
@@ -1,5 +1,8 @@
 use_bpm 155
 use_synth :piano
+line_1="C:/Users/windgrove_daniel/Downloads/Audacity/I_Choose_You.wav"
+line_2="C:/Users/windgrove_daniel/Downloads/Audacity/Greninja!.wav"
+ending="C:/Users/windgrove_daniel/Downloads/Audacity/WINNER.wav"
 
 v= 0.25
 #first time plays quiet
@@ -29,214 +32,112 @@ end
 
 #third time gets louder
 
+live_loop :notes do
+  play :d2
+  sleep 0.5
+  play :f2
+  sleep 0.5
+  play :g2, sustain: 7
+  sleep 1.5
+  
+  play :d3
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :d3, sustain: 1
+  sleep 1.5
+  play :d3
+  sleep 0.5
+  play :c3, sustain: 0.5
+  sleep 1
+  play :a2
+  sleep 0.5
+  play :f2, sustain: 1.5
+  sleep 2
+  
+  # "Like no one ever was..."
+  play :f3
+  sleep 0.5
+  play :d2
+  sleep 0.5
+  play :g2, sustain: 3
+  sleep 1
+  play :d3, sustain: 0.5
+  sleep 1
+  play :d3, sustain: 0.5
+  sleep 1
+  play :c3, sustain: 0.5
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  
+  play :c3, sustain: 6
+  sleep 2.5
+  play :g2, sustain: 2
+  sleep 1
+  play :c2, sustain: 2
+  sleep 1
+  play :d2, sustain: 2
+  sleep 0.5
+  play :ds2, sustain: 6
+  sleep 2
+  play :ds4, sustain: 0.5
+  sleep 0.5
+  play :ds4, sustain: 0.5
+  sleep 0.5
+  play :ds4, sustain: 0.5
+  sleep 0.5
+  play :ds4, sustain: 4
+  sleep 1
+  play :ds4, sustain: 0.5
+  sleep 1
+  play :d3, sustain: 0.5
+  sleep 1
+  play :c3, sustain: 0.5
+  sleep 0.5
+  play :as2, sustain: 9
+  sleep 2
+  play :as2, sustain: 1
+  sleep 0.5
+  play :d3, sustain: 1
+  sleep 1
+  play :d3
+  sleep 0.5
+  play :c3, sustain: 1
+  sleep 1
+  play :as3
+  sleep 0.5
+  play :d3,sustain: 7
+  sleep 4
+  play :g2, sustain: 9
+  sleep 2
+  stop
+end
 
-play :d2
-sleep 0.5
-play :f2
-sleep 0.5
-play :g2, sustain: 7
-sleep 1.5
+sleep 15
 
-play :d3
-sleep 0.5
-play :d3
-sleep 0.5
-play :d3
-sleep 0.5
-play :d3, sustain: 1
-sleep 1.5
-play :d3
-sleep 0.5
-play :c3, sustain: 0.5
-sleep 1
-play :a2
-sleep 0.5
-play :f2, sustain: 1.5
-sleep 2
+test = 0
+live_loop :drums do
+  22.times do
+    print test
+    test = test + 1
+    sample :drum_cymbal_closed
+    sleep 1
+  end
+  stop
+end
 
-play :f3
-sleep 0.5
-play :d2
-sleep 0.5
-play :g2, sustain: 3
-sleep 1
-play :d3, sustain: 0.5
-sleep 1
-play :d3, sustain: 0.5
-sleep 1
-play :c3, sustain: 0.5
-sleep 0.5
-play :bs2
-sleep 0.5
-
-play :c3, sustain: 6
-sleep 2.5
-play :g2, sustain: 2
-sleep 1
-play :c2, sustain: 2
-sleep 1
-play :d2, sustain: 2
-sleep 0.5
-play :ds2, sustain: 6
-sleep 2
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 4
-sleep 1
-play :ds4, sustain: 0.5
-sleep 1
-play :d3, sustain: 0.5
-sleep 1
-play :c3, sustain: 0.5
-sleep 0.5
-play :as2, sustain: 9
-sleep 2
-play :as2, sustain: 1
-sleep 1
-play :d3, sustain: 1.5
-sleep 1
-play :d3, sustain: 1
-sleep 1
-play :c3, sustain: 1.5
-sleep 1
-play :as2, sustain: 0.5
-sleep 0.5
-play :d3, sustain: 6
-sleep 4
-play :g2, sustain: 6
-sleep 1.5
-play :d3
-sleep 0.5
-play :d3
-sleep 0.5
-play :d3
-sleep 0.5
-play :d3, sustain: 1
-sleep 1.5
-play :d3
-sleep 0.5
-play :c3, sustain: 1
-sleep 1
-play :a2
-sleep 0.5
-play :f2, sustain: 3
-sleep 1.5
-play :d2
-sleep 0.5
-play :f2
-sleep 0.5
-play :g2, sustain: 7
-sleep 1.5
-play :d3
-sleep 0.5
-play :d3, sustain: 1
-sleep 1
-play :c3, sustain: 1
-sleep 1
-play :as3
-sleep 0.5
-play :c3, sustain: 6
-sleep 3
-play :g2, sustain: 2
-sleep 1
-play :c2, sustain: 2
-sleep 1
-play :d2, sustain: 2
-sleep 0.5
-play :ds2, sustain: 6
-sleep 2
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 0.5
-sleep 0.5
-play :ds4, sustain: 4
-sleep 1
-play :ds4, sustain: 0.5
-sleep 0.5
-play :d2, sustain: 1
-sleep 1
-play :c3
-sleep 0.5
-play :as2, sustain: 4
-sleep 2
-play :as2
-sleep 0.5
-play :d4
-sleep 0.5
-play :f4
-sleep 0.5
-play :g4, sustain: 1.5
-sleep 1.5
-play :d4, sustain: 1
-sleep 1
-play :f4
-sleep 0.5
-play :g4, sustain: 1.5
-sleep 1
-play :g4, sustain: 1.5
-sleep 1
-play :f4, sustain: 1
-sleep 1
-play :d4, sustain: 1.5
-sleep 1
-play :c4
-sleep 0.5
-play :as3, sustain: 4
-sleep 3
-play :g4
-sleep 0.5
-play :g4
-sleep 1
-play :a4
-sleep 1
-play :as4
-sleep 1
-play :a4, sustain: 1.5
-sleep 1
-play :g4, sustain: 1.5
-sleep 1
-play :f4, sustain: 3
-sleep 2
-play :g4, sustain: 3
-sleep 1.5
-play :g4
-sleep 0.5
-play :f4
-sleep 0.5
-play :g4, sustain: 3
-sleep 0.5
-play :g4
-sleep 0.5
-play :f4
-sleep 0.5
-play :d4, sustain: 1.5
-sleep 1
-play :c4, sustain: 2
-sleep 1.5
-play :as3
-sleep 0.5
-play :c4
-sleep 0.5
-play :as2, sustain: 3
-sleep 1
-play :d4, sustain: 2
-sleep 1
-play :d4
-sleep 0.5
-play :c4, sustain: 1.5
-sleep 1
-play :as3
-sleep 0.5
-play :d4, sustain: 3
-sleep 3
-play :d4, sustain: 3
-sleep 3
+sleep 11
 
 
-
+live_loop :soundeffects do
+  sample line_1, amp: 1
+  sleep 12
+  sample line_2,amp: 1
+  sleep 10
+  sample ending
+  sleep 8
+  stop
+end
